7d5c84342e6315946a8959276e9bd8998c1b9fef
KoubaJirkal <kuba.jirkal@gmail.com>
2026-05-13 21:11:22 +0200

    automatic migrastion

--- SemestralniPraceSTIN/Program.cs
+++ SemestralniPraceSTIN/Program.cs
@@ -89,6 +89,8 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+    db.Database.Migrate();
+
     if (!db.User.Any())
     {
         db.User.Add(new User
